1768083cc1212435f8cc7f4e7b8917164dfff5d2
Ivy Basseches <imb2139@columbia.edu>
2022-11-11 19:17:48 -0500

    New functionality
    - Pass selective args from the old function using "[NUMBER]".
    - Option to edit in-place, or to output to a different file.

--- replace.py
+++ replace.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import re
 import sys
 
 import clang.cindex
@@ -17,49 +18,77 @@ def find_func(tu, name):
       calls.append(c)
   return calls
 
-def replace_call(token_gen, filename, fn_name, fn_args, offset):
+def replace_call(o_file, call, content, fn_name, fn_args, offset):
   """ Replaces call to old function with call to a new function. Optionally
   replaces the old function args with args passed in as CLA.
   """
   tokens = []
-  for token in token_gen:
+  for token in call.get_tokens():
     tokens.append(token)
+
+  # Extract args.
+  old_args = []
+  for arg_cursor in call.get_arguments():
+    start = arg_cursor.extent.start.offset
+    end = arg_cursor.extent.end.offset
+    old_args.append(content[start:end])
+
+  for idx, arg in enumerate(old_args):
+    fn_args = re.sub(f"\[{re.escape(str(idx))}\]", arg, fn_args)
 
   # The first token is always the function ID (i.e. name).
   old_id = tokens[0]
   start_id_offset = old_id.extent.start.offset
-  diff = start_id_offset - offset
+
+  # Initialize with the end of the function ID
+  end_offset = old_id.extent.end.offset
 
   # Open the file and write from last offset until reaching the function name,
   # then write the function name.
-  with open(filename, "r") as c_file:
-    c_file.seek(offset)
-    sys.stdout.write(c_file.read(diff))
+  if o_file:
+    with open(o_file, "a") as output:
+      output.write(content[offset:start_id_offset])
+      output.write(fn_name)
+
+      # If user provided args to replace, do that.
+      if fn_args:
+        output.write(f"({fn_args}")
+  else:
+    sys.stdout.write(content[offset:start_id_offset])
     sys.stdout.write(fn_name)
 
-  end_offset = old_id.extent.end.offset
+    # Ditto
+    if fn_args:
+      sys.stdout.write(f"({fn_args}")
 
-  # If user provided args to replace, do that.
   if fn_args:
-    sys.stdout.write(f"({fn_args}")
     # The last token is now the close paren.
     end_offset = tokens[-1].extent.start.offset
 
   # Update the offset value
   return end_offset
 
-def replace_calls(calls, filename, fn_name, fn_args):
+def replace_calls(o_file, calls, filename, fn_name, fn_args):
   """Replaces all calls to a function."""
+  with open(filename, "r") as c_file:
+    content = c_file.read()
+
+  # Want to overwrite the output file
+  if o_file:
+    open(o_file, "w").close()
+
   curr_offset = 0
   for call in calls:
     # Replace calls, writing parts of the file to stdout along the way.
-    curr_offset = replace_call(call.get_tokens(), filename, fn_name, fn_args,
-                               curr_offset)
+    curr_offset = replace_call(o_file, call, content, fn_name,
+                               fn_args, curr_offset)
 
   # Write out the rest of the file
-  with open(filename, "r") as c_file:
-    c_file.seek(curr_offset)
-    sys.stdout.write(c_file.read())
+  if o_file:
+    with open(o_file, "a") as output:
+      output.write(content[curr_offset:])
+  else:
+    sys.stdout.write(content[curr_offset:])
 
 class ParseFnArgs(argparse.Action):
   def __call__(self, parser, namespace, args, option_string=None):
@@ -85,14 +114,26 @@ def replace():
                       metavar='<newfn>',
                       type=str,
                       help='the new function\'s name')
-  
+
+  # Can pass arguments as $number for old args
   parser.add_argument('-a',
                       '--args',
                       nargs='*',
                       action=ParseFnArgs,
-                      help='new args to be passed to the function')
-  
+                      help='new args to be passed to the function (refer to'
+                      ' old function\'s args with [number])')
+
+  parser.add_argument('-i',
+                      action="store_true",
+                      help='in place edit, if specified')
+
+  parser.add_argument('-o',
+                      '--outputfile',
+                      action="store",
+                      help='the name of an output file, if specified')
+
   args = parser.parse_args()
+
   filename = args.filepath
   if not os.path.isfile(filename):
     print('The file specified does not exist')
@@ -112,7 +153,8 @@ def replace():
 
   new_fn = args.newfn
   fn_args = args.args
-  replace_calls(calls, filename, args.newfn, args.args)
+  o_file = filename if args.i else args.outputfile
+  replace_calls(o_file, calls, filename, args.newfn, args.args)
 
 if __name__ == '__main__':
   replace()
