a8146429efb3a9753770f3e54b8be228808f14a3
Ivy Basseches <imb2139@columbia.edu>
2023-01-05 16:52:21 -0500

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,162 +1,185 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 import re
 import sys
+from pathlib import Path
 
 import clang.cindex
+from termcolor import cprint
+
+def is_valid_function(call, tu, name):
+    if call.location.file is None or call.location.file.name != tu.spelling:
+        return False
+    is_func_call = call.kind == clang.cindex.CursorKind.CALL_EXPR
+    if is_func_call and (call.spelling == name):
+        return True
+    return False
 
 def find_func(tu, name):
-  """ Retrieve lists of call expressions in a translation unit."""
-  calls = []
-  for c in tu.cursor.walk_preorder():
-    if c.location.file is None:
-      continue
-    is_func_call = c.kind == clang.cindex.CursorKind.CALL_EXPR
-    if is_func_call and (c.spelling == name):
-      calls.append(c)
-  return calls
+    """Retrieve lists of function declarations and call expressions in a
+    translation unit.
+    """
+    return [ c for c in tu.cursor.walk_preorder() if is_valid_function(c, tu, name) ]
+
 
 def replace_call(o_file, call, content, fn_name, fn_args, offset):
-  """ Replaces call to old function with call to a new function. Optionally
-  replaces the old function args with args passed in as CLA.
-  """
-  tokens = []
-  for token in call.get_tokens():
-    tokens.append(token)
+    """ Replaces call to old function with call to a new function. Optionally
+    replaces the old function args with args passed in as CLA.
+    """
+    if debug:
+        cprint(f"[ Replacing call to {call.spelling}() on line "
+            f"{call.extent.start.line} of {call.location.file}. ]", "red")
 
-  if fn_args:
-    # Extract old args
-    old_args = []
-    for arg_cursor in call.get_arguments():
-      start = arg_cursor.extent.start.offset
-      end = arg_cursor.extent.end.offset
-      old_args.append(content[start:end])
+    tokens = []
+    for token in call.get_tokens():
+        tokens.append(token)
 
-    # Replace [NUMBER] with old function arg
-    for idx, arg in enumerate(old_args):
-      fn_args = re.sub(f"\[{re.escape(str(idx))}\]", arg, fn_args)
+    # Where the function call starts
+    start_offset = tokens[0].extent.start.offset
 
-  # The first token is always the function ID (i.e. name).
-  old_id = tokens[0]
-  start_id_offset = old_id.extent.start.offset
-
-  # Initialize with the end of the function ID
-  end_offset = old_id.extent.end.offset
-
-  # Open the file and write from last offset until reaching the function name,
-  # then write the function name.
-  if o_file:
-    with open(o_file, "a") as output:
-      output.write(content[offset:start_id_offset])
-      output.write(fn_name)
-
-      # If user provided args to replace, do that.
-      if fn_args:
-        output.write(f"({fn_args}")
-  else:
-    sys.stdout.write(content[offset:start_id_offset])
-    sys.stdout.write(fn_name)
-
-    # Ditto
     if fn_args:
-      sys.stdout.write(f"({fn_args}")
+        # Extract old args
+        old_args = []
+        for arg_cursor in call.get_arguments():
+            arg_start = arg_cursor.extent.start.offset
+            arg_end = arg_cursor.extent.end.offset
+            old_args.append(content[arg_start:arg_end])
 
-  if fn_args:
-    # The last token is now the close paren.
-    end_offset = tokens[-1].extent.start.offset
+        # Replace [NUMBER] with old function arg
+        for idx, arg in enumerate(old_args):
+            fn_args = re.sub(f"\[{re.escape(str(idx))}\]", arg, fn_args)
 
-  # Update the offset value
-  return end_offset
+        args_string = f"({fn_args})"
+
+    else:
+        # The second token is the open paren.
+        open_paren = tokens[1].extent.start.offset
+        args_string = content[open_paren:tokens[-1].extent.end.offset]
+
+    # Chunk to write (previous offset + new name and args)
+    offset_chunk = content[offset:start_offset]
+    new_call = fn_name + args_string
+
+    # Open the file and write from last offset until reaching the function name,
+    # then write the function name.
+    if o_file:
+        if debug:
+            cprint("[ Old function call ]", "blue")
+            print(content[start_offset:tokens[-1].extent.end.offset])
+            cprint("[ New function call ]", "blue")
+            print(new_call)
+
+        with open(o_file, "a") as output:
+            output.write(offset_chunk + new_call)
+    else:
+        sys.stdout.write(offset_chunk + new_call)
+
+    # Update the offset value
+    return tokens[-1].extent.end.offset
 
 def replace_calls(o_file, calls, filename, fn_name, fn_args):
-  """Replaces all calls to a function."""
-  with open(filename, "r") as c_file:
-    content = c_file.read()
+    """Replaces all calls to a function."""
+    with open(filename, "rb") as c_file:
+        # To avoid any weird characters
+        content = c_file.read().decode("mac_roman")
 
-  # Overwrite the output file
-  if o_file:
-    open(o_file, "w").close()
+    # Overwrite the output file
+    if o_file:
+        open(o_file, "w").close()
 
-  curr_offset = 0
-  for call in calls:
-    # Replace calls, writing parts of the file to stdout along the way.
-    curr_offset = replace_call(o_file, call, content, fn_name, fn_args,
-                               curr_offset)
+    curr_offset = 0
+    for call in calls:
+        # Replace calls, writing parts of the file to stdout along the way.
+        curr_offset = replace_call(o_file, call, content, fn_name, fn_args,
+                                curr_offset)
 
-  # Write out the rest of the file
-  if o_file:
-    with open(o_file, "a") as output:
-      output.write(content[curr_offset:])
-  else:
-    sys.stdout.write(content[curr_offset:])
+    # Write out the rest of the file
+    if o_file:
+        with open(o_file, "a") as output:
+            output.write(content[curr_offset:])
+    else:
+        sys.stdout.write(content[curr_offset:])
 
 class ParseFnArgs(argparse.Action):
-  def __call__(self, parser, namespace, args, option_string=None):
-    delim = ", "
-    setattr(namespace, self.dest, delim.join(args))
+    def __call__(self, parser, namespace, args, option_string=None):
+        delim = ", "
+        setattr(namespace, self.dest, delim.join(args))
 
 def replace():
-  parser = argparse.ArgumentParser(
-    description='Find and replace function calls. Writes result to stdout.'
-  )
+    parser = argparse.ArgumentParser(
+        description='Find and replace function calls. Writes result to stdout'
+                    ' by default.'
+    )
 
-  parser.add_argument('filepath',
-                      metavar='<filepath>',
-                      type=str,
-                      help='the path of the .c file')
+    parser.add_argument('filepath',
+                        type=str,
+                        help='the path of the .c file to search',
+                        metavar='<filename>')
 
-  parser.add_argument('oldfn',
-                      metavar='<oldfn>',
-                      type=str,
-                      help='the name of the function to replace')
+    parser.add_argument('old_fn',
+                        type=str,
+                        help='the name of the function to replace',
+                        metavar='<old-fn>')
 
-  parser.add_argument('newfn',
-                      metavar='<newfn>',
-                      type=str,
-                      help='the new function\'s name')
+    parser.add_argument('new_fn',
+                        type=str,
+                        help='the new function\'s name',
+                        metavar='<new-fn>')
 
-  parser.add_argument('-a',
-                      '--args',
-                      nargs='*',
-                      action=ParseFnArgs,
-                      help='new args to be passed to the function (refer to'
-                      ' old function\'s args with [number])')
+    parser.add_argument('-a', '--args',
+                        nargs='*',
+                        action=ParseFnArgs,
+                        help='new args to be passed to the function (refer to'
+                        ' old function\'s args with [number])',
+                        metavar='args')
 
-  parser.add_argument('-i',
-                      '--inplace',
-                      action="store_true",
-                      help='in place edit, if specified')
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help='print some debug information')
 
-  parser.add_argument('-o',
-                      '--outputfile',
-                      action="store",
-                      help='the name of an output file, if specified')
+    parser.add_argument('-f', '--create-file',
+                        action='store_true',
+                        help='touch a file with the old function name if the'
+                        ' function was replaced')
 
-  args = parser.parse_args()
+    parser.add_argument('-i', '--in-place',
+                        action='store_true',
+                        help='in place edit, if specified')
 
-  filename = args.filepath
-  if not os.path.isfile(filename):
-    print('The file specified does not exist')
-    sys.exit(1)
+    parser.add_argument('-o', '--output-file',
+                        action='store',
+                        help='the name of an output file, if specified',
+                        metavar='<filename>')
 
-  idx = clang.cindex.Index.create()
+    args = parser.parse_args()
 
-  # Generate translation unit
-  tu = idx.parse(filename)
+    global debug
+    debug = args.debug
 
-  # Return cursor that points to the function call
-  calls = find_func(tu, args.oldfn)
+    filename = args.filepath
+    if not Path(filename).is_file():
+        cprint('The file specified does not exist.', 'red')
+        sys.exit(1)
 
-  if len(calls) == 0:
-    # Function call wasn't found
-    sys.exit(1)
+    idx = clang.cindex.Index.create()
 
-  new_fn = args.newfn
-  fn_args = args.args
-  o_file = filename if args.inplace else args.outputfile
-  replace_calls(o_file, calls, filename, args.newfn, args.args)
+    # Generate translation unit
+    tu = idx.parse(filename)
+
+    # Return cursor that points to the function call
+    calls = find_func(tu, args.old_fn)
+
+    if len(calls) == 0:
+        # Function call wasn't found
+        cprint('The function specified was not found.', 'red')
+        sys.exit(0)
+
+    if args.create_file:
+        Path(f".{args.old_fn}").touch()
+
+    o_file = filename if args.in_place else args.output_file
+    replace_calls(o_file, calls, filename, args.new_fn, args.args)
 
 if __name__ == '__main__':
-  replace()
+    replace()
